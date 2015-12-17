1f5b77db07c98ba4fed249eca82d8ce414a86794
Ryan Tablada <ryan.tablada@gmail.com>
2015-12-16 20:40:03 -0600

    More API JSCS updates

--- resource/templates/_api.js
+++ resource/templates/_api.js
@@ -1,31 +1,30 @@
 // Module dependencies.
 var express = require('express');
 var router = express.Router();
-var mongoose = require('mongoose');
 var api = {};
 
 // ALL
-api.<%= pluralName %> = function (req, res) {
+api.<%= pluralName %> = function(req) {
   return req.store.recordCollection('<%= capSchemaName %>');
 };
 
 // GET
-api.<%= lowSchemaName %> = function (req, res) {
+api.<%= lowSchemaName %> = function(req) {
   return req.store.recordItemById('<%= capSchemaName %>', req.params.id);
 };
 
 // POST
-api.add<%= capSchemaName %> = function (req, res) {
+api.add<%= capSchemaName %> = function(req) {
   return req.store.createRecord('<%= capSchemaName %>');
 };
 
 // PUT
-api.edit<%= capSchemaName %> = function (req, res) {
+api.edit<%= capSchemaName %> = function(req) {
   return req.store.updateRecord('<%= capSchemaName %>', req.params.id);
 };
 
 // DELETE
-api.delete<%= capSchemaName %> = function (req, res) {
+api.delete<%= capSchemaName %> = function(req) {
   return req.store.destroyRecord('<%= capSchemaName %>', req.params.id);
 };
 
@@ -36,6 +35,5 @@ router.route('/<%= lowSchemaName %>/:id')
   .get(api.<%= lowSchemaName %>)
   .put(api.edit<%= capSchemaName %>)
   .delete(api.delete<%= capSchemaName %>);
-
 
 module.exports = router;
