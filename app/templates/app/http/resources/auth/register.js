404dcc20c17d3d2d4d32617e8cfb28b2775b6e27
Ryan Tablada <ryan.tablada@gmail.com>
2016-01-05 12:21:41 -0600

    [feat] Register OAuth2 Password compliant-ish

--- app/templates/app/http/resources/auth/register.js
+++ app/templates/app/http/resources/auth/register.js
@@ -1,26 +1,32 @@
+/* jshint camelcase: false */
 var express = require('express');
 var router = express.Router();
 var User = require('mongoose').model('User');
+var oauth = require('../../../oauth');
 
-/* GET login form. */
-router.post('/', function(req, res) {
-  var email = req.getJson('data.attributes.email');
-  var password = req.getJson('data.attributes.password');
-  var dataType = req.getJson('data.type');
+function createUser(req, res, next) {
+  // jscs: disable
+  var email = req.body.username;
+  var password = req.body.password;
+  var userData = JSON.parse(req.body.user_data);
+  userData.email = email;
+  userData.password = password;
+  req.body.grant_type = req.body.grant_type || 'password';
+  // jscs: enable
 
-  if (dataType !== 'user-registration') {
-    return res.status(400).send({message: `Unmatched data-type: "${dataType}" should be "user-registration"`});
-  }
-
-  User.create({email, password}, function(err) {
+  User.create(userData, function(err) {
     if (err && err.code === 11000) {
-      return res.send('A user already exists with this email.');
+      return res.status(400)
+        .send('A user already exists with this email.');
     } else if (err) {
       return res.send(err);
     } else {
-      res.send('success');
+      next();
     }
   });
-});
+}
+
+/* GET login form. */
+router.post('/', createUser, oauth.grant());
 
 module.exports = router;
